b9c1fc00ca5a29274cef2f36a8cfd462f6b06cf9
Roma <mukhin2804@gmail.com>
2018-10-10 00:55:21 +0300

    change verification

--- index.php
+++ index.php
@@ -5,37 +5,39 @@
 
 		$window = ' class="window"';
 
-		function convertor($seconds)	{
-
+				
+		function conva($seconds)	{
+			$sec = 0;
 			define('SEC_IN_HOUR', 3600);
 			define('SEC_IN_MIN', 60);
 
 			$sec = $seconds % SEC_IN_MIN;
 
-			$h = ($seconds >= SEC_IN_HOUR && function_exists('intdiv')) ?	$h = intdiv($seconds, SEC_IN_HOUR) :	$h = number_format($seconds / SEC_IN_HOUR);
+			$h = ($seconds >= SEC_IN_HOUR && function_exists('intdiv')) ? intdiv($seconds, SEC_IN_HOUR) :	(int) ($seconds / SEC_IN_HOUR);
 					
-			$min = (function_exists('intdiv')) ?	$min = intdiv($seconds, SEC_IN_MIN) :	$min = ($seconds - $sec) / SEC_IN_MIN;
+			$min = (function_exists('intdiv')) ? intdiv($seconds, SEC_IN_MIN) : (int) $seconds / SEC_IN_MIN;
 				
 					if ($h >= 1) {
-						$min = $min % SEC_IN_MIN;
+						$min %= SEC_IN_MIN;
 					}
 
-			$h = ($h < 9) ? '0' . $h : $h;
+			$h = ($h < 10) ? '0' . $h : $h;
 			$min = ($min < 10) ? '0' . $min : $min;
 			$sec = ($sec < 10) ? '0'.$sec : $sec;
 				return "H $h : M $min : S $sec";
-		
 		}
+				if ($num > 359999) {
+					echo "<h1$window>Too big to fall</h1>";
+				} else {
+					echo "<h2$window>" . conva($num) . '</h2>';
+				}
 
 } else {
-	$h = 0;
-	$min = 0;
-	$sec = 0;
-}
-		echo "<h2$window>" .convertor($num) . '</h2>';
-?>
-
+					$h = 0;
+					$min = 0;
+					$sec = 0;
+}?>
 <form action="index.php" method="post" class="form">
-		<input placeholder="input number" type="text" name="num" id="put" maxlength="6">
+		<input placeholder="input number" type="text" name="num" maxlength="6">
 		<input type="submit" name="sub">
 </form>
